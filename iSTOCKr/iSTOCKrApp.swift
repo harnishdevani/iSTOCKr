8dcd278aa7f65869437b7e2fc50abb68968de008
harnishdevani <harnishdevani03@gmail.com>
2024-12-19 08:56:25 +0100

    Initial Commit

---
+++ iSTOCKr/iSTOCKrApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSTOCKrApp.swift
+//  iSTOCKr
+//
+//  Created by Harnish Devani on 19/12/24.
+//
+
+import SwiftUI
+
+@main
+struct iSTOCKrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
